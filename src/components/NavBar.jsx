a2e29ec8b7b2bb0d4f3a5c089daea84d91f653f1
Vijaypaanagala <paanagalavijaykumar@gmail.com>
2024-06-28 15:58:32 +0530

    Resolved merge conflicts

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import "../Styles/NavBar.css"
+
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Logo</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Find Work</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Find Talent</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Events</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <a className="nav-link" href="#">Login</a>
+            <button type="button" className="btn btn-success custom-btn">Sign up</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
