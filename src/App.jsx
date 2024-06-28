fdadd8167a29674a482178c8e792111857bfd038
Lohitha0801 <lohitha0801@gmail.com>
2024-06-28 16:29:06 +0530

    first change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import NavBar from "./components/NavBar"
+import Section from "./components/Section"
 function App() {
   
   return (
+    <div>
   <NavBar/>
+  <Section/>
+  </div>
+
   )
    
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,9 @@
+function Section() {
+    return (  
+        <div>
+            <p>hello world</p>
+        </div>
+    );
+}
+
+export default Section;
